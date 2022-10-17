b54830234c566ed33f86b9665ba3ed0f0045526b
ItamarJanuario <juniorlemos15421@gmail.com>
2022-10-17 12:17:35 -0300

    removendo testes antigos para acrescentar novos

--- contaCorrente/src/test/java/ContaCorrenteTest.java
+++ contaCorrente/src/test/java/ContaCorrenteTest.java
@@ -3,43 +3,37 @@ import com.digytal.contaCorrente.Contas.Conta;
 import com.digytal.contaCorrente.Contas.ContaCorrente;
 import com.digytal.contaCorrente.exception.DataException;
 import com.digytal.contaCorrente.exception.ValorIncorretoException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
 
 public class ContaCorrenteTest {
 
+    LocalDate dataTeste;
+    LocalDate dataTesteFuture;
+    Cliente clienteTeste;
+    Cliente clienteTeste2;
+    ContaCorrente contaCorrente;
+    ContaCorrente contaCorrente2;
 
-    @DisplayName("Fazendo testes de todos os metodos sacar")
-    @Test
-    void TestandoMetodoSacar(){
+    @BeforeEach
+    public void setUp() {
+        dataTeste = LocalDate.now();
+        dataTesteFuture = LocalDate.now().plusMonths(1);
+
+        clienteTeste = new Cliente("Itamar Januario Teste", dataTeste, "122-444-666.80");
+        clienteTeste = new Cliente("Jessica Antenas Parabolicas", dataTeste.plusMonths(2), "111-432-656.10");
+
+        contaCorrente = new ContaCorrente(clienteTeste, 123, 323, clienteTeste.getCpf());
+        contaCorrente2 = new ContaCorrente(clienteTeste2, 232, 355, clienteTeste2.getCpf());
 
     }
 
-
-    @DisplayName("Fazendo teste do metodo sacar da Conta Corrente")
-    @Test
-    void TestandoMetodoSacarContaCorrente() throws ValorIncorretoException {
-        LocalDate dataNascimento = LocalDate.parse("2001-03-07");
-        Cliente clienteTest = new Cliente("Itamar", dataNascimento, "124.741.574-80");
-        ContaCorrente contaTest = new ContaCorrente(clienteTest, 123, 322, clienteTest.getCpf());
-
-        Double expectativa = 100.0;
-
-        Assertions.assertEquals(expectativa, resultado);
+    @Nested
+    @DisplayName("Testando todas as funcionalidades do metodoSacar")
+    public class TesteMetodoSaque {
+        
     }
 
-    @DisplayName("Testando Exception de Data")
-    @Test
-    void testando_exception_DataException() throws ValorIncorretoException{
-        Conta contaTest = new Conta();
-          contaTest.depositar(100.0);
-
-        Assertions.assertThrows(DataException.class, () -> {
-           contaTest.consultarExtrato(LocalDate.parse("2022-12-20"), LocalDate.parse("2022-12-12"));
-        });
-    }
 
 }
